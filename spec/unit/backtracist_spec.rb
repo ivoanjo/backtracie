0af3f99184f9ff02837c73ea395a2aaec75f0fd0
Ivo Anjo <ivo@ivoanjo.me>
2021-03-10 20:42:10 +0000

    Refactor spec to enable future testing with different stack test cases without repetion

--- spec/unit/backtracist_spec.rb
+++ spec/unit/backtracist_spec.rb
@@ -20,109 +20,81 @@
 
 require "backtracist"
 
-RSpec.describe Backtracist do
-  describe ".caller_locations" do
-    let(:caller_locations) { described_class.caller_locations }
-    let(:kernel_locations) { Kernel.caller_locations }
+require "unit/interesting_backtrace_helper"
 
+RSpec.describe Backtracist do
+  shared_examples "an equivalent of the Ruby API (using locations)" do
     it "returns an array of Backtracist::Location instances" do
-      expect(caller_locations).to all(be_a(Backtracist::Location))
+      expect(backtracist_stack).to all(be_a(Backtracist::Location))
     end
 
-    it "returns the same number of items as Kernel.caller_locations" do
-      expect(caller_locations.size).to be kernel_locations.size
+    it "returns the same number of items as the Ruby API" do
+      expect(backtracist_stack.size).to be ruby_stack.size
     end
 
     describe "each returned Backtracist::Location" do
-      it "has the same absolute_path as the corresponding Kernel.caller_locations entry" do
-        caller_locations.zip(kernel_locations).each do |backtracist_location, kernel_location|
+      it "has the same absolute_path as the corresponding Ruby API entry" do
+        backtracist_stack.zip(ruby_stack).each do |backtracist_location, kernel_location|
           expect(backtracist_location.absolute_path).to eq kernel_location.absolute_path
         end
       end
 
-      it "has the same base_label as the corresponding Kernel.caller_locations entry" do
-        caller_locations.zip(kernel_locations).each do |backtracist_location, kernel_location|
+      it "has the same base_label as the corresponding Ruby API entry" do
+        backtracist_stack.zip(ruby_stack).each do |backtracist_location, kernel_location|
           expect(backtracist_location.base_label).to eq kernel_location.base_label
         end
       end
 
-      it "has the same label as the corresponding Kernel.caller_locations entry" do
-        caller_locations.zip(kernel_locations).each do |backtracist_location, kernel_location|
+      it "has the same label as the corresponding Ruby API entry" do
+        backtracist_stack.zip(ruby_stack).each do |backtracist_location, kernel_location|
           expect(backtracist_location.label).to eq kernel_location.label
         end
       end
 
-      it "has the same lineno as the corresponding Kernel.caller_locations entry" do
-        caller_locations.zip(kernel_locations).each do |backtracist_location, kernel_location|
+      it "has the same lineno as the corresponding Ruby API entry" do
+        backtracist_stack.zip(ruby_stack).each do |backtracist_location, kernel_location|
           expect(backtracist_location.lineno).to eq kernel_location.lineno
         end
       end
 
-      it "has the same path as the corresponding Kernel.caller_locations entry" do
-        caller_locations.zip(kernel_locations).each do |backtracist_location, kernel_location|
+      it "has the same path as the corresponding Ruby API entry" do
+        backtracist_stack.zip(ruby_stack).each do |backtracist_location, kernel_location|
           expect(backtracist_location.path).to eq kernel_location.path
         end
       end
     end
   end
 
+  describe ".caller_locations" do
+    context "when sampling the rspec stack" do
+      let(:backtracist_stack) { described_class.caller_locations }
+      let(:ruby_stack) { Kernel.caller_locations }
+
+      it_should_behave_like "an equivalent of the Ruby API (using locations)"
+    end
+  end
+
   describe ".backtrace_locations" do
-    let!(:backtraces_for_comparison) {
-      # These two procs should never be reformatted to be on different lines!
-      #
-      # This code is laid out like this so that the full stack traces obtained by backtracist and the regular Ruby APIs
-      # match exactly. Initially I had used two lets: `let(:backtrace_locations) { Thread.new(...).value }` and
-      # `let(:thread_locations) { Thread.new(...).value }` but because they were in separate lines, the resulting
-      # stack traces differed -- because they pointed at two different lines!
-      #
-      # Hence this weird trick of declaring the two procs on the same line, so that the stack traces are similar, even
-      # though the code paths followed are different.
-      # I solemnly promise to only use my evil powers for good, or something.
-      [proc { described_class.backtrace_locations(Thread.main) }, proc { Thread.main.backtrace_locations }]
-        .map { |target_proc| Thread.new(&target_proc).value }
-    }
+    let(:backtracist_stack) { backtraces_for_comparison.first }
+    let(:ruby_stack) { backtraces_for_comparison.last }
 
-    let(:backtrace_locations) { backtraces_for_comparison.first }
-    let(:thread_locations) { backtraces_for_comparison.last }
+    context "when sampling the rspec stack from another thread" do
+      let!(:backtraces_for_comparison) {
+        # These two procs should never be reformatted to be on different lines!
+        #
+        # This code is laid out like this so that the full stack traces obtained by backtracist and the regular Ruby APIs
+        # match exactly. Initially I had used two lets: `let(:backtrace_locations) { Thread.new(...).value }` and
+        # `let(:thread_locations) { Thread.new(...).value }` but because they were in separate lines, the resulting
+        # stack traces differed -- because they pointed at two different lines!
+        #
+        # Hence this weird trick of declaring the two procs on the same line, so that the stack traces are similar, even
+        # though the code paths followed are different.
+        # I solemnly promise to only use my evil powers for good, or something.
+        [proc { described_class.backtrace_locations(Thread.main) }, proc { Thread.main.backtrace_locations }]
+          .map { |target_proc| Thread.new(&target_proc).value }
+      }
 
-    it "returns an array of Backtracist::Location instances" do
-      expect(backtrace_locations).to all(be_a(Backtracist::Location))
-    end
-
-    it "returns the same number of items as Thread.backtrace_locations" do
-      expect(backtrace_locations.size).to be thread_locations.size
-    end
-
-    describe "each returned Backtracist::Location" do
-      it "has the same absolute_path as the corresponding Thread.backtrace_locations entry" do
-        backtrace_locations.zip(thread_locations).each do |backtracist_location, kernel_location|
-          expect(backtracist_location.absolute_path).to eq kernel_location.absolute_path
-        end
-      end
-
-      it "has the same base_label as the corresponding Thread.backtrace_locations entry" do
-        backtrace_locations.zip(thread_locations).each do |backtracist_location, kernel_location|
-          expect(backtracist_location.base_label).to eq kernel_location.base_label
-        end
-      end
-
-      it "has the same label as the corresponding Thread.backtrace_locations entry" do
-        backtrace_locations.zip(thread_locations).each do |backtracist_location, kernel_location|
-          expect(backtracist_location.label).to eq kernel_location.label
-        end
-      end
-
-      it "has the same lineno as the corresponding Thread.backtrace_locations entry" do
-        backtrace_locations.zip(thread_locations).each do |backtracist_location, kernel_location|
-          expect(backtracist_location.lineno).to eq kernel_location.lineno
-        end
-      end
-
-      it "has the same path as the corresponding Thread.backtrace_locations entry" do
-        backtrace_locations.zip(thread_locations).each do |backtracist_location, kernel_location|
-          expect(backtracist_location.path).to eq kernel_location.path
-        end
-      end
+      it_should_behave_like "an equivalent of the Ruby API (using locations)"
     end
 
     context "when first argument is not a thread" do
@@ -133,10 +105,10 @@ RSpec.describe Backtracist do
 
     context "when first argument is a subclass of thread" do
       let(:thread_subclass) { Class.new(Thread) }
-      let(:backtrace_locations) { thread_subclass.new { described_class.backtrace_locations(Thread.current) }.value }
+      let(:backtracist_stack) { thread_subclass.new { described_class.backtrace_locations(Thread.current) }.value }
 
       it "returns an array of Backtracist::Location instances" do
-        expect(backtrace_locations).to all(be_a(Backtracist::Location))
+        expect(backtracist_stack).to all(be_a(Backtracist::Location))
       end
     end
   end
