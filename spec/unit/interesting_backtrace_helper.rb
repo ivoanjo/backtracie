9f72048ed49616fd05187be637da11ea21b709a1
Ivo Anjo <ivo@ivoanjo.me>
2021-03-14 17:22:56 +0000

    Add license header to interesting_backtrace_helper.rb

--- spec/unit/interesting_backtrace_helper.rb
+++ spec/unit/interesting_backtrace_helper.rb
@@ -1,3 +1,23 @@
+# frozen_string_literal: true
+
+# backtracie: Ruby gem for beautiful backtraces
+# Copyright (C) 2021 Ivo Anjo <ivo@ivoanjo.me>
+#
+# This file is part of backtracie.
+#
+# backtracie is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# backtracie is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with backtracie.  If not, see <http://www.gnu.org/licenses/>.
+
 # The following tries to reproduce the most interesting (contrived?) call stack I can think of, with as many weird
 # variants as possible.
 #
